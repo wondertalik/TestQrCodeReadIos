e9c2d45d503030ec44645e10aab8ce827e73ea48
Vitalii Korestkyi <vitalii@sto.ad>
2019-05-03 15:31:47 +0200

    Initial Commit

---
+++ TestQrCodeReadIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestQrCodeReadIos
+//
+//  Created by Vitalii Koretskyi on 5/3/19.
+//  Copyright © 2019 Vitalii Koretskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
